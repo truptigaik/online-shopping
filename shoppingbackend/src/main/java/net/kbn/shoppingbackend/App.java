d35aaeaf2cd40fddea3632ab56f8727586405366
truptigaik <truptigaikwad0726@gmail.com>
2020-04-28 16:04:40 +0530

    my project setup completed successsfully

---
+++ shoppingbackend/src/main/java/net/kbn/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.kbn.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
